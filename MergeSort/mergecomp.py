d7ba43ff8c8a9ae403698c628358d0ae54bb96ef
rahulsahay114 <54280955+rahulsahay114@users.noreply.github.com>
2021-01-17 11:47:27 +0530

    updated scripts and data.

--- MergeSort/mergecomp.py
+++ MergeSort/mergecomp.py
@@ -1,10 +1,7 @@
 import random as r
 import math
 import matplotlib.pyplot as plt
-tot = [] #list of number of comparisons made in different runs of insertion sort.
-inps = []
-
-ListOfInpSizeAndComps = []
+import csv 
 
 def mergeSort(arr):
     
@@ -37,6 +34,7 @@ def mergeSort(arr):
                 i += 1
 
             else:
+                noOfComp += 1
                 arr[k] = R[j]
                 j += 1
             k += 1
@@ -54,45 +52,129 @@ def mergeSort(arr):
 
     return noOfComp
 
-def plot(lst):
+def plot(lstBest, lstAvg, lstWorst):
     
-    x = [] 
-    y = [] 
+    xAvg = [] 
+    yAvg = [] 
+
+    xBest = [] 
+    yBest = [] 
+
+    xWorst = [] 
+    yWorst = [] 
+
     yLog = []
 
-    lst.sort() #sorting by size of inputs.
+    lstAvg.sort() #sorting by size of inputs.
+    lstBest.sort() #sorting by size of inputs.
+    lstWorst.sort() #sorting by size of inputs.
     
-    for i in range(len(lst)):
-        x.append(lst[i][0]) #'size of inputs' extracted.
-        y.append(lst[i][1])
+    for i in range(len(lstAvg)):
+        xAvg.append(lstAvg[i][0]) #'size of inputs' extracted.
+        yAvg.append(lstAvg[i][1])
 
+    for i in range(len(lstBest)):
+        xBest.append(lstBest[i][0]) #'size of inputs' extracted.
+        yBest.append(lstBest[i][1])
 
-    plt.plot(x,y)
-    
-    plt.legend(['Average', 'nlogn'])
+    for i in range(len(lstWorst)):
+        xWorst.append(lstWorst[i][0]) #'size of inputs' extracted.
+        yWorst.append(lstWorst[i][1])
+
+    for i in range(len(xAvg)):
+        yLog.append(xAvg[i]*math.log(xAvg[i],2))
+
+    plt.plot(xAvg,yAvg)
+    plt.plot(xBest,yBest, 'r--')
+    plt.plot(xWorst,yWorst, 'y')
+    plt.plot(xAvg, yLog, 'g.-')
+    plt.legend(['Average Case', 'Best Case', 'Worst Case', 'nlogn'])
     plt.xlabel('Size of Input')
     plt.ylabel('Number of comparisions')
     plt.grid(True)
     plt.show()
 
-def main():
+def avg_case():
+    
+    ListOfInpSizeAndComps = [] #list of number of comparisons made in different runs of insertion sort.
 
-    noOfRuns = r.randint(200, 300) #deciding the total number of runs to be made
+    noOfRuns = r.randint(10, 100) #deciding the total number of runs to be made
 
     while(noOfRuns > 0):
         
         x = 0
-        noOfInp = r.randint(100, 2000) #input size
+        noOfInp = r.randint(0, 600) #input size
         a = [int(i*r.random()) for i in range(noOfInp)]
         x = mergeSort(a) #comparisions
 
-        ListOfInpSizeAndComps.append((noOfInp, x))
+        ListOfInpSizeAndComps.append((noOfInp, x)) #list of tuples.
         
         noOfRuns -= 1
 
-    #print('ListOfInpSizeAndComps: \n{}'.format(ListOfInpSizeAndComps))
-    #print(a)
+    #print('List of Comparisons made in each run: \n{}'.format(tot))
+    #print('\nTotal Runs: {}'.format(len(tot)))
 
-    plot(ListOfInpSizeAndComps)
+    return ListOfInpSizeAndComps
+
+def worst_case():
+    
+    ListOfInpSizeAndComps = [] #list of number of comparisons made in different runs of insertion sort.
+
+    noOfRuns = r.randint(10, 100) #deciding the total number of runs to be made
+
+    while(noOfRuns > 0):
+        
+        x = 0
+        noOfInp = r.randint(0, 600) #input size
+        a = [int(i*r.random()) for i in range(noOfInp)]
+        a.sort(reverse = True)
+        x = mergeSort(a) #comparisions
+
+        ListOfInpSizeAndComps.append((noOfInp, x)) #list of tuples.
+        
+        noOfRuns -= 1
+
+    return ListOfInpSizeAndComps
+
+def best_case():
+
+    ListOfInpSizeAndComps = [] #list of number of comparisons made in different runs of insertion sort.
+
+    noOfRuns = r.randint(10, 100) #deciding the total number of runs to be made
+
+    while(noOfRuns > 0):
+        
+        x = 0
+        noOfInp = r.randint(0, 600) #input size
+        a = [int(i*r.random()) for i in range(noOfInp)]
+        a.sort()
+        x = mergeSort(a) #comparisions
+
+        ListOfInpSizeAndComps.append((noOfInp, x)) #list of tuples.
+        
+        noOfRuns -= 1
+
+    return ListOfInpSizeAndComps
+
+def main():
+
+    lstAvg = avg_case()
+    lstBest = best_case()
+    lstWorst = worst_case()
+
+    plot(lstBest, lstAvg, lstWorst) 
+
+    lstAvg.insert(0, 'AVG(InpSize,Cmprsns)')
+    lstBest.insert(0, 'BEST(InpSize,Cmprsns)')
+    lstWorst.insert(0, 'WORST(InpSize,Cmprsns)')
+
+    allLists = [lstAvg, lstWorst, lstBest]
+
+    with open('data_mergesort.csv', 'w') as file:
+            writer = csv.writer(file)
+            writer.writerows(allLists)
+
+
 
 main()
+
