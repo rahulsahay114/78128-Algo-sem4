7614638e15c09a0f869b6b40727b0db27d876ae3
rahulsahay114 <54280955+rahulsahay114@users.noreply.github.com>
2021-04-20 08:34:16 +0530

    Update SubsetSum.py

--- Weighted Interval Scheduling & Subset Sum/SubsetSum.py
+++ Weighted Interval Scheduling & Subset Sum/SubsetSum.py
@@ -17,21 +17,21 @@ for i in range(n+1):
 for i in range(W+1):
 	M[0][i] = 0
 
-#row, starting from the row next 
+#starting from the row next 
 #to the last row which was initialised to 0
 #i.e we begin from 1 to n
 for i in range(1, n+1):
 	
-	#row, starting from the column next 
+	#starting from the column next 
 	#to the last column which was initialised to 0
-	#i.e we begin from 1 to n
+	#i.e we begin from 1 to W
 	for j in range(1, W+1):
 		
 		#w[i-1] because index is offset by 1, w[1] is w[0]
 		#as we start for-loop for 'i' from 1.
 		#remove -1 from i-1 to understand the algo.
 		if j < w[i-1]:
-			M[i][j] = M[i][j]
+			M[i][j] = M[i-1][j]
 
 		else:
 			M[i][j] = max(M[i-1][j], w[i-1] + M[i-1][j- w[i-1]])
